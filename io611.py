e38b596477f639e7b469ace4a22a823a9cd425a0
Aran-lq <liqi9281@gmail.com>
2017-10-18 23:38:49 +0800

    Simple completely version

--- io611.py
+++ io611.py
@@ -3,13 +3,21 @@
 # Created by Aran-lq, 2017/10/18
 import sys, os, pickle
 
-def func(key, value):
+def func(key, value, flag):
     if os.path.exists('./WareData.pkl'):
         pkl_file = open('WareData.pkl','rb')
         WareHouseData = pickle.load(pkl_file)
     else:
         WareHouseData = {}
-    WareHouseData[key] = value
+    if (flag=='-'):
+        tmp = int(WareHouseData[key]) - int(value)
+        WareHouseData[key] = str(tmp)
+    else:
+        if (WareHouseData.has_key(key)):
+            tmp = int(WareHouseData[key]) + int(value)
+            WareHouseData[key] = str(tmp)
+        else:
+            WareHouseData[key] = value
     print ("Following are the things in 607:\n")
     for i in range(len(WareHouseData)):
         print (WareHouseData.items()[i])
@@ -19,7 +27,7 @@ def func(key, value):
     print "Completed!"
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 3): 
+    if (len(sys.argv) != 3) and (len(sys.argv)!=4):
         print "Error value, usage: io607 [key] [value]"
         exit()
-    func(sys.argv[1], sys.argv[2])
+    func(sys.argv[1], sys.argv[2], sys.argv[3])
