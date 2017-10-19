8f3c936d4655994716cc351c976e9f83b74fe36d
Aran-lq <liqi9281@gmail.com>
2017-10-19 10:42:51 +0800

    update

---
+++ io607.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2.7
+# io607 - A simple program to record info of warehouse in and out
+# Created by Aran-lq, 2017/10/18
+import sys, os, pickle
+
+def func(key, value, flag=0):
+	if os.path.exists('./WareData.pkl'):
+		pkl_file = open('WareData.pkl','rb')
+		WareHouseData = pickle.load(pkl_file)
+	else:
+		WareHouseData = {}
+	if (flag=='-'):
+		tmp = int(WareHouseData[key]) - int(value)
+		WareHouseData[key] = str(tmp)
+	else:
+		if (WareHouseData.has_key(key)):
+			tmp = int(WareHouseData[key]) + int(value)
+			WareHouseData[key] = str(tmp)
+		else:
+			WareHouseData[key] = value
+	print ("Following are the things in 607:\n")
+	for i in range(len(WareHouseData)):
+		print (WareHouseData.items()[i])
+	output = open('WareData.pkl','wb')
+	pickle.dump(WareHouseData, output)
+	output.close()
+	print "Completed!"
+
+if __name__ == "__main__":
+	if (len(sys.argv) != 3) and (len(sys.argv)!=4):
+		print "Error value, usage: io607 [key] [value]"
+		exit()
+
+	print len(sys.argv)
+	if len(sys.argv) == 4:	  
+		func(sys.argv[1], sys.argv[2], sys.argv[3])
+	else:
+		func(sys.argv[1], sys.argv[2])
